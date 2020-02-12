c5ca57d16f04ad6550cb61c6062ae8a3b904494b
Jnorman2123 <Jnorman2123@gmail.com>
2020-02-12 10:25:22 -0500

    added seed for two leauges

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+leagues = League.create([{name: 'English Premier League', logo: 'https://a1.espncdn.com/combiner/i?img=%2Fi%2Fleaguelogos%2Fsoccer%2F500%2F23.png', country: 'England', division: 1}, 
+    {name: 'LaLiga Santander', logo: 'https://upload.wikimedia.org/wikipedia/en/3/35/La_Liga.png', country: 'Spain', division: 1}])
