aa3f5df2226071af70b231af3cdc80b8b78dc301
Jnorman2123 <Jnorman2123@gmail.com>
2020-02-12 11:21:08 -0500

    added seed data for 2 players

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,6 @@ leagues = League.create([{name: 'English Premier League', logo: 'https://a1.espn
 
 teams = Team.create([{name: 'Chelsea F.C.', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/180px-Chelsea_FC.svg.png', nickname: 'The Blues, The Pensioners', league_id: 1, stadium: 'Stamford Bridge', manager: 'Frank Lampard', year_founded: 1905},
     {name: 'Futbol Club Barcelona', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/180px-FC_Barcelona_%28crest%29.svg.png', nickname: 'Barca, Blaugrana', league_id: 2, stadium: 'Camp Nou', manager: 'Quique Setien', year_founded: 1899}])
+
+ players = Player.create([{name: 'Lionel Messi', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Lionel_Messi_20180626.jpg/220px-Lionel_Messi_20180626.jpg', nationality: 'Argentina', appearances: 713, goals: 622, market_value: 154, team_id: 2, jersey_number: 10},
+    {name: 'Christian Pulisic', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/USMNT_vs._Trinidad_and_Tobago_%2848125059622%29_%28cropped%29.jpg/220px-USMNT_vs._Trinidad_and_Tobago_%2848125059622%29_%28cropped%29.jpg', nationality: 'United States of America', appearances: 23, goals: 6, market_value: 66, team_id: 1, jersey_number: 22}])   
