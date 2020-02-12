fea1ebb0f9377772359b491bbd41f10b9f47afdd
Jnorman2123 <Jnorman2123@gmail.com>
2020-02-12 09:59:57 -0500

    created team scaffold

---
+++ db/migrate/20200212145916_create_teams.rb
@@ -0,0 +1,12 @@
+class CreateTeams < ActiveRecord::Migration[6.0]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.string :logo
+      t.string :nickname
+      t.references :league, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
